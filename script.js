1e86f3b8609821b40f0bd46fe79c5f30dad30c7d
ababacarsedikh.gueyebg437490-web <khadidiatoudiasse46@gmail.com>
2026-04-23 16:17:39 -0300

    Modification dans le fichier script.js

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    
-    // 1. Sélection des éléments du formulaire
-    const form = document.querySelector('form');
-    const btnEnvoyer = form.querySelector('button[type="submit"]');
 
-    // boutton envoyez
-    form.addEventListener('submit', (e) => {
-        // Empêche la page de se recharger (comportement par défaut du HTML)
+    // le formulaire dans contact.html
+    const monFormulaire = document.querySelector('form');
+
+    monFormulaire.addEventListener('submit', (e) => {
+       
         e.preventDefault();
 
-        // recuperation des utulisateur
-        const données = {
-            nom: document.getElementById('Nom').value,
-            prenom: document.getElementById('Prenom').value,
-            email: document.getElementById('Email').value,
-            message: document.getElementById('Message').value
-        };
+        const nom = document.getElementById('Nom').value;
+        const prenom = document.getElementById('Prenom').value;
+        const email = document.getElementById('Email').value;
 
-        
-        const texteOriginal = btnEnvoyer.innerText;
-        btnEnvoyer.innerText = "Envoi en cours...";
-        btnEnvoyer.disabled = true; // Désactive le bouton pendant l'envoi
-        btnEnvoyer.style.opacity = "0.7";
+        // Affichage du message de confirmation
+        alert("Succès !\n\nBonjour " + prenom + " " + nom + ",\nVotre mail (" + email + ") a bien été reçu par notre équipe.");
 
-        
-        setTimeout(() => {
-            // Affichage d'un message de succès personnalisé
-            alert(`Merci ${données.prenom} ! Votre message a bien été envoyé au restaurant.`);
-
-            // Réinitialisation du formulaire et du bouton
-            form.reset();
-            btnEnvoyer.innerText = texteOriginal;
-            btnEnvoyer.disabled = false;
-            btnEnvoyer.style.opacity = "1";
-            
-            console.log("Données envoyées :", données);
-        }, 1500);
-
-        
+        //validation du message de confirmation
+        monFormulaire.reset();
     });
 });
